4948ba98e5e37b59a12e5b5299549598a01333ea
Marcelo Boschi <marceloboschi78@hotmail.com>
2025-06-05 15:00:42 -0300

    feat: testes unitarios com moq

--- DevFreela.UnitTests/Application/ProjectDeleteHandlerTests.cs
+++ DevFreela.UnitTests/Application/ProjectDeleteHandlerTests.cs
@@ -1,6 +1,7 @@
 ﻿using DevFreela.Application.CQRS.Commands;
 using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
+using Moq;
 using NSubstitute;
 
 namespace DevFreela.UnitTests.Application
@@ -51,6 +52,51 @@ namespace DevFreela.UnitTests.Application
             Assert.True(result.Message == ProjectDeleteCommandHandler.PROJECT_NOT_FOUND_MESSAGE);
             await repository.Received(1).GetById(Arg.Any<int>());
             await repository.DidNotReceive().Update(Arg.Any<Project>());
+        }
+
+        [Fact]
+        public async Task ProjectExists_Delete_Success_Moq()
+        {
+            //arrange            
+            var project = new Project("Projeto A", "Projeto A descrição", 1, 2, 10000);
+
+            var repository = Mock.Of<IProjectRepository>(r =>
+                r.GetById(It.IsAny<int>()) == Task.FromResult((Project)project) &&
+                r.Update(It.IsAny<Project>()) == Task.CompletedTask);                        
+
+            var handler = new ProjectDeleteCommandHandler(repository);
+
+            var command = new ProjectDeleteCommand(1);
+
+            //act
+            var result = await handler.Handle(command, new CancellationToken());
+
+            //assert
+            Assert.True(result.IsSuccess);
+            Mock.Get(repository).Verify(r => r.GetById(It.IsAny<int>()), Times.Once);
+            Mock.Get(repository).Verify(r => r.Update(It.IsAny<Project>()), Times.Once);            
+        }
+
+        [Fact]
+        public async Task ProjectDoesNotExist_Delete_Error_Moq()
+        {
+            //arrange            
+            var repository = Mock.Of<IProjectRepository>(r =>
+                r.GetById(It.IsAny<int>()) == Task.FromResult((Project?)null) &&
+                r.Update(It.IsAny<Project>()) == Task.CompletedTask);
+
+            var handler = new ProjectDeleteCommandHandler(repository);
+
+            var command = new ProjectDeleteCommand(1);
+
+            //act
+            var result = await handler.Handle(command, new CancellationToken());
+
+            //assert
+            Assert.False(result.IsSuccess);
+            Assert.True(result.Message == ProjectDeleteCommandHandler.PROJECT_NOT_FOUND_MESSAGE);
+            Mock.Get(repository).Verify(r => r.GetById(It.IsAny<int>()), Times.Once);
+            Mock.Get(repository).Verify(r => r.Update(It.IsAny<Project>()), Times.Never);            
         }
     }
 }
--- DevFreela.UnitTests/Application/ProjectInsertHandlerTests.cs
+++ DevFreela.UnitTests/Application/ProjectInsertHandlerTests.cs
@@ -30,17 +30,59 @@ namespace DevFreela.UnitTests.Application
 
             var mediator = Substitute.For<IMediator>();
             mediator.Publish(Arg.Any<ProjectCreatedNotification>(), Arg.Any<CancellationToken>()).Returns(Task.CompletedTask);
-            
+
             var handler = new ProjectInsertCommandHandler(mediator, repository);
-            
+
             //act
             var result = await handler.Handle(command, new CancellationToken());
-            
+
             //assert
             Assert.True(result.IsSuccess);
             Assert.Equal(ID, result.Data);
             await repository.Received(1).Add(Arg.Any<Project>());
             await mediator.Received(1).Publish(Arg.Any<ProjectCreatedNotification>(), Arg.Any<CancellationToken>());
+        }
+
+        [Fact] // utilizando Moq
+        public async Task InputDataAreOk_Insert_Success_Moq()
+        {
+            //arrange
+            const int ID = 1;
+
+            //var mock = new Mock<IProjectRepository>();
+            //mock.Setup(r => r.Add(It.IsAny<Project>())).ReturnsAsync(ID);
+            //var repository = mock.Object;
+
+            var repository = Mock.Of<IProjectRepository>(r => r.Add(It.IsAny<Project>()) == Task.FromResult(ID));
+
+            var command = new ProjectInsertCommand
+            {
+                Title = "Projeto A",
+                Description = "Descricao do Projeto",
+                IdClient = 1,
+                IdFreelancer = 2,
+                TotalCost = 10000
+            };
+
+            //var mock2 = new Mock<IMediator>();
+            //mock2.Setup(m => m.Publish(It.IsAny<ProjectCreatedNotification>(), It.IsAny<CancellationToken>())).Returns(Task.CompletedTask);
+            //var mediator = mock2.Object;    
+
+            var mediator = Mock.Of<IMediator>(m => m.Publish(It.IsAny<ProjectCreatedNotification>(), It.IsAny<CancellationToken>()) == Task.CompletedTask);
+            
+            var handler = new ProjectInsertCommandHandler(mediator, repository);
+
+            //act
+            var result = await handler.Handle(command, new CancellationToken());
+
+            //assert
+            Assert.True(result.IsSuccess);
+            Assert.Equal(ID, result.Data);
+            
+            //mock.Verify(r => r.Add(It.IsAny<Project>()), Times.Once);
+            Mock.Get(repository).Verify(r => r.Add(It.IsAny<Project>()), Times.Once);
+            Mock.Get(mediator).Verify(m => m.Publish(It.IsAny<ProjectCreatedNotification>(), It.IsAny<CancellationToken>()), Times.Once);
+
         }
     }
 }
